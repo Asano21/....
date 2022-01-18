e0cb170f9859db6bd9d62d1804cda2098e15c87f
Asano21 <timonasan@gmail.com>
2022-01-18 18:53:58 +0600

    list

---
+++ js/script.js
@@ -0,0 +1,40 @@
+// homewoork 1
+/* let fruit = ['banana', 'apple', 'strawberry', 'grape', 'watermelon']
+let cost = [150, 200, 100, 175, 500]
+for (let i = 0; i <= fruit.length - 1; i++) {
+    console.log(fruit[i] + ' costs ', cost[i]);
+ } */
+
+// homework 2
+/* let arr = []
+for (let i = 0; i < 10; i++) {
+    let random = Math.round(Math.random() * 100);
+    arr.push(random)
+}
+console.log(arr) */
+
+// homework 3
+/* let arr = []
+while (true) {
+    let name = prompt('Type name: ');
+    if (name == 'exit') {
+        alert('We stopped loop XD')
+        break
+    } 
+    arr.push(name);
+    console.log(`Hello ${name}`);
+};
+console.log(arr) */
+
+// homewrok 4 
+// var names = ['Anelya', 'Vlad', 'Ersyn', 'Temirlan', 'Bagjan']
+// var toDeleteName = prompt('Type to delete name: ');
+// var index = names.indexOf(toDeleteName);
+// console.log(names, 'Before')
+// for (let i = 0; i < names.length; i++) {
+//     if (index == i) {
+//         names.splice(i, 1)
+//     } 
+// }
+
+// console.log(names, 'After')
